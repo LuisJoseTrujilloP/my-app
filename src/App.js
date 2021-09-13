3cf72dcda6d9a83210f691cbff8ccac79b6c2b73
LuisJoseTrujilloP <luisjosetrujillodeveloper@gmail.com>
2021-09-13 11:30:44 -0500

    this is the v2.0

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          esta es una app para aprender a usar Git
+          esta es una app para aprender a usar Git v2.0
         </p>
         <a
           className="App-link"
