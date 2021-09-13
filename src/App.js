8dd907d002afd420b0aaab24ba0e449683a4b7ee
LuisJoseTrujilloP <luisjosetrujillodeveloper@gmail.com>
2021-09-13 09:37:19 -0500

    A diff version of the app

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          esta es una app para aprender a usar Git
         </p>
         <a
           className="App-link"
